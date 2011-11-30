4daa0d0da1ba376b3b50f67b3598bf4d83b1796d
Tiago Antao <tiagoantao@gmail.com>
2011-11-30 21:14:47 +0000

    pipeline refactoring - ongoing

--- phasing/do_something_CHRwise.pl
+++ phasing/do_something_CHRwise.pl
@@ -37,7 +37,7 @@ my $tprefix;
 my $tlog;
 for $k ($chr .. $ARGV[1]){
     if ($ARGV[2] eq "AFRO"){
-	$log=$k."/afro".$k.".farmput";
+	$log=$k."_afro".$k.".farmput";
 	$tlog="afro".$k.".farmput";
 	$out=$k."/AFRO_phase".$k;
 	$inp=$k."/AFRICAN_chr".$k.".recode.phase.inp";
@@ -63,12 +63,12 @@ for $k ($chr .. $ARGV[1]){
 	$out=$k."/OOA_phase".$k;
 	$inp=$k."/OOA_chr".$k.".recode.phase.inp";
 	$known=$k."/CEUtrios_chr".$k."_knownHaplotypes.txt";
-	$ped=$k."/OOA_chr".$k.".ped";
-	$map=$k."/OOA_chr".$k.".map";
-	$bgl=$k."/OOA_chr".$k.".bgl";
-	$pedtrios=$k."/OOAtrios_chr".$k.".ped";
-	$maptrios=$k."/OOAtrios_chr".$k.".map";
-	$bgltrios=$k."/OOAtrios_chr".$k.".bgl";
+	$ped=$k."_OOA_chr".$k.".ped";
+	$map=$k."_OOA_chr".$k.".map";
+	$bgl=$k."_OOA_chr".$k.".bgl";
+	$pedtrios=$k."_OOAtrios_chr".$k.".ped";
+	$maptrios=$k."_OOAtrios_chr".$k.".map";
+	$bgltrios=$k."_OOAtrios_chr".$k.".bgl";
 	$markers=$k."/markers_chr".$k.".txt";
 	$phased= $k."/CEUtrios_chr".$k.".bgl";
 	$unphased=$k."/OOA_chr".$k.".bgl";
@@ -78,8 +78,8 @@ for $k ($chr .. $ARGV[1]){
     }
     #system "bsub -P team19 -o $tlog 'plink --tfile $tprefix --keep-allele-order --recode --tab --noweb --out $tprefix'"#transpose tped into bed
     if ($ARGV[3] eq "Ped2Beagle"){
-	system "ped_to_bgl $ped $map > $bgl"; #get ped into beagle format
-	system "ped_to_bgl $pedtrios $maptrios > $bgltrios"; #get ped into beagle format
+	system "ped_to_bgl SplitUnphased/$ped SplitUnphased/$map > Ped2Beagle/$bgl"; #get ped into beagle format
+	system "ped_to_bgl SplitUnphased/$pedtrios SplitUnphased/$maptrios > Ped2Beagle/$bgltrios"; #get ped into beagle format
     }
     if ($ARGV[3] eq "Markers"){
 	system "cut -f 2,4,5,6 $k/tomarkers.bim > $k/markers_chr$k.txt"; #get testbim files into beagle markers file
