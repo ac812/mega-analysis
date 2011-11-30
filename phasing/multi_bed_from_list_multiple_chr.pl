83895ee276e1e089953af4d27ea5aec561d523e9
Tiago Antao <tr353@elephant.bio.cam.ac.uk>
2011-11-30 17:26:48 +0000

    pipeline refactoring - ongoing

--- phasing/multi_bed_from_list_multiple_chr.pl
+++ phasing/multi_bed_from_list_multiple_chr.pl
@@ -3,24 +3,23 @@
 
 use strict;
 #$ARGV[4] is the column where the splitting label is
+#$ARGV[5] is the output directory
 my $i;
 my $j;
 my $k;
 my $last=0;
 my @split;
 
+my $outDir=$ARGV[5];
+
 open (LIST, "<$ARGV[0]");
 my @lines= <LIST>;
 chomp @lines;
 my $chr=$ARGV[2];
-open (POPS, ">pops_downstream.txt");
+open (POPS, ">$outDir/pops_downstream.txt");
 my $count=0;
 for $k ($chr .. $ARGV[3]){
     $count =0;
-    system "mkdir $k";
-    #my $tobecut=  $k."_Genetic_Distances_and_ANCESTRAL_DAFs.txt";
-    #my $aftercut= $k."_snplist.txt";
-    #system "cut -f 2 $tobecut > $aftercut";
     
     for $i (0 .. $#lines){
 	@split = split(/\s+/, $lines[$i]);
@@ -30,7 +29,7 @@ for $k ($chr .. $ARGV[3]){
 	    if ($ARGV[4] == 3 and $k == $ARGV[2]){
 		print POPS "$split[$ARGV[4]]\t";
 	    }
-	    my $outfile= "$k/".$i.$pop."_temp.txt";
+	    my $outfile= "$outDir/${k}_".$i.$pop."_temp.txt";
 	    print STDERR "$outfile";
 	    #die;
 	    open (OUT, ">$outfile");
@@ -42,43 +41,22 @@ for $k ($chr .. $ARGV[3]){
 	    my $log=$pop.".farmput";
 	   #my $extract= $aftercut; #$k."_Genetic_Distances_and_ANCESTRAL_DAFs.txt";
 	    if ($ARGV[4] == 2){
-		system "plink --bfile $ARGV[1] --keep $outfile --chr $k --keep-allele-order --recode --alleleACGT --tab --out $k/$pop --noweb";
+		system "plink --bfile $ARGV[1] --keep $outfile --chr $k --keep-allele-order --recode --alleleACGT --tab --out $outDir/${k}_$pop --noweb";
 		if ($count ==0){
 		    
-		    system "plink --bfile $ARGV[1] --keep $outfile --chr $k --keep-allele-order --make-bed --out $k/tomarkers --noweb";
+		    system "plink --bfile $ARGV[1] --keep $outfile --chr $k --keep-allele-order --make-bed --out $outDir/${k}_tomarkers --noweb";
 		    $count=1;
 		}
 	    }
 	    if ($ARGV[4] == 3){
-		system "plink --bfile $ARGV[1] --keep $outfile --chr $k --keep-allele-order --recode-fastphase --reference-allele ancestral_alleles.txt --out $k/$pop --noweb";
+		system "plink --bfile $ARGV[1] --keep $outfile --chr $k --keep-allele-order --recode-fastphase --reference-allele ancestral_alleles.txt --out $outDir/${k}_$pop --noweb";
 	    }
-	    
-	    
-	    
-	    #system "bsub -o $k/$log -P team19 'plink --bfile $ARGV[1] --keep $outfile --chr $k --hardy --out $k/$pop --noweb --reference-allele ~lp8/TOOMAS_SAFE/rs_ids_safe_snps_ANCESTRAL_STATE.txt'";
 	    close (OUT);
-	    
 	}
-	
     }
     $last=0;
     $i=0;
 }
 print POPS "\n";
 close (POPS);
-#system "rm *temp.txt *.nosex *.log";
 close (LIST);
-	
-
-	
-
-
-
-
-
-
-
-
-
-
-
