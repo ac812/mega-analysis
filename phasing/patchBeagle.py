2d12d3606e6282bf70b667b4715a0d9a849837e5
Tiago Antao <tr353@elephant.bio.cam.ac.uk>
2011-12-01 12:47:33 +0000

    patch beagle input files

--- phasing/patchBeagle.py
+++ phasing/patchBeagle.py
@@ -1,6 +1,19 @@
+import os
 import sys
 
-for l in sys.stdin:
-    toks = l.split("\t")[::2]
-    print "\t".join(toks),
-    
+if len(sys.argv)==1:
+    path = "."
+else:
+    path = sys.argv[1]
+
+files = os.listdir(path)
+for file in files:
+    if file.find("trios")==-1:
+        continue
+    core = path + os.sep + file
+    r = open(core)
+    w = open(core + ".W", "w")
+    for l in r:
+        toks = l.split("\t")[::2]
+        w.write("\t".join(toks))
+    os.rename(core + ".W", core)
